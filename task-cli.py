7bb57e4a08b5904b6aab88797fa51af3a8ba4372
0x0zgdo <zgdotech@gmail.com>
2026-04-26 22:29:37 +0800

    add task feature added

--- task-cli.py
+++ task-cli.py
@@ -1,4 +1,50 @@
 import sys
+import json
+import os
 
-print(sys.argv)
+TASKS_FILE = "tasks.json"
+
+def load_tasks(): 
+    if not os.path.exists(TASKS_FILE):
+        return []
+    
+    with open(TASKS_FILE, "r") as file:
+        return json.load(file)
+    
+
+def save_task(tasks):
+    with open(TASKS_FILE, "w") as file:
+        json.dump(tasks, file, indent=2)
+
+
+def get_next_id(tasks):
+    if len(tasks) == 0:
+        return 1
+    
+    max_id = 0
+
+    for task in tasks:
+        if task["id"] > max_id:
+             max_id = task["id"]
+
+    return max_id + 1
+
+
+
+command = sys.argv[1]
+
+if command == "add":
+    if len(sys.argv) < 3:
+        print("Error: missing task description")
+    else:
+        description = sys.argv[2]
+        tasks = load_tasks()
+        task = {
+            "id": get_next_id(tasks),
+            "description": description,
+            "status": "todo"
+        }
+        tasks.append(task)
+        save_task(tasks)
+        print("Task added successfully")
 
